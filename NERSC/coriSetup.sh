bcfd3dcfbca6c3ba383994fbb589f738f4e6eab7
Tom Glanzman <dragon@slac.stanford.edu>
2017-11-13 13:55:47 -0800

    Disable DC1 visitDB and install temporarily hard-wired four instanceCats for development

--- NERSC/coriSetup.sh
+++
@@ -1,8 +0,0 @@
-# coriSetup.sh - cori-specific configs for phoSim and DMTCP
-module swap PrgEnv-intel PrgEnv-gnu
-module swap gcc gcc/5.2.0
-module rm craype-network-aries
-module rm cray-libsci
-module unload craype
-module load python/2.7-anaconda
-export CC=gcc
